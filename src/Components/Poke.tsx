52e60154cb0527a82910218c0b878b490376ca5f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-16 22:30:28 +0100

    :rocket: :tada:   pokeAPI  :rocket: :tada:

--- src/Components/Poke.tsx
+++ src/Components/Poke.tsx
@@ -3,7 +3,7 @@ import "./Poke.css"
 
 interface Pokemon{
     name: string;
-    sprites: string;
+    image: string;
 }
 
 export const Poke = () => {
@@ -14,10 +14,23 @@ export const Poke = () => {
             try {
                 const response= await fetch('https://pokeapi.co/api/v2/pokemon/')
                 const data = await response.json();
-                console.log(response);
                 
+                const pokemonList: Pokemon[] =[];
+
+                const fetchPokemonDetails = async (result :{name: string; url: string}) => {
+                    const pokemonResponse = await fetch(result.url);
+                    const pokemonData = await pokemonResponse.json();
+
+                    pokemonList.push({
+                        name: result.name,
+                        image: pokemonData.sprites.front_default,
+                    });
+                }
+
+                data.results.forEach(fetchPokemonDetails);
+                setPokemonData(pokemonList)
             } catch (error) {
-                
+                console.error('Error fetching Pokemon data:', error);
             }
         }
         fetchData();
